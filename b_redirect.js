c67d65120762c459211a29e895f093ca7e70d797
Eric Gundrum <git@ericgundrum.com>
2017-02-24 18:08:25 -0800

    Additional logging. Output remains the same.

--- b_redirect.js
+++ b_redirect.js
@@ -3,3 +3,6 @@ console.log('begin building b_redirect')
 module.exports = require('./b')
 
 console.log('finished building b_redirect', module.exports)
+if (!module.exports.get_c) {
+  console.log('=== notice b_redirect is empty')
+}
--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 
+console.log('--- require b_redirect')
 var redirect = require('./b_redirect')
+console.log('--- end first require\n')
 
 console.log('This is redirect; it should match what was built by b:', redirect)
 if (redirect) console.log('redirect.c should be object:', redirect.c)
 if (redirect.c) console.log('redirect.c.root should be object:', redirect.c.root)
 
+console.log('--- require root')
 var root = require('./root')
+console.log('--- end second require\n')
 
 console.log('root should be object:', root)
+if (!root.b.get_c) {
+  console.log('=== notice root.b is empty')
+}
 console.log('redirect should match what was built by b:', redirect)
